61667c0163bfd42ed5f5e49b55fcfcaf28c96be9
Anders Sundman <anders@4zm.org>
2017-03-12 19:42:41 +0100

    Styling using elm-mdl

--- webapp/src/Catalog.elm
+++ webapp/src/Catalog.elm
@@ -1,8 +1,38 @@
 module Catalog exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (Html, div, text)
+import Material.List as List
+import Material.Card as Card
+import Material.Elevation as Elevation
+import Material.Color as Color
+
+
+itemView : String -> Html msg
+itemView plant =
+    List.li
+        []
+        [ List.content [] [ text plant ] ]
 
 
 view : Html msg
 view =
-    text "This is the Catalog page"
+    let
+        plants =
+            [ "Foo"
+            , "Fubar"
+            , "Bar"
+            ]
+    in
+        div []
+            [ text "This is the Catalog page"
+            , List.ul [] (List.map itemView plants)
+            , Card.view
+                [ Elevation.e2 ]
+                [ Card.title
+                    [ Color.background Color.primary
+                    , Color.text Color.white
+                    ]
+                    [ Card.head [] [ text "CardHead" ] ]
+                , Card.text [] [ text "Text" ]
+                ]
+            ]
--- webapp/src/Main.elm
+++ webapp/src/Main.elm
@@ -1,19 +1,32 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Navigation exposing (Location)
-import Nav
-import Routing
 import Catalog exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (href, class, style)
+import Material
+import Material.Button as Button
+import Material.Layout as Layout
+import Material.Options as Options exposing (css)
+import Material.Scheme
+import Navigation exposing (newUrl, Location)
+import Routing exposing (..)
 
 
 type alias Model =
-    { route : Routing.Route }
+    { route : Routing.Route
+    , mdl : Material.Model
+    }
+
+
+type alias Mdl =
+    Material.Model
 
 
 type Msg
     = NoOp
+    | SetRoute Route
     | OnLocationChange Location
+    | Mdl (Material.Msg Msg)
 
 
 init : Location -> ( Model, Cmd Msg )
@@ -22,7 +35,10 @@ init location =
         currentRoute =
             Routing.parseLocation location
     in
-        { route = currentRoute } ! []
+        { route = currentRoute
+        , mdl = Material.model
+        }
+            ! []
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -38,30 +54,81 @@ update msg model =
             in
                 { model | route = newRoute } ! []
 
+        SetRoute route ->
+            model ! [ newUrl <| urlFor route ]
+
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
+subscriptions model =
+    Sub.batch ([ Material.subscriptions Mdl model ])
+
+
+viewDrawer : Model -> Html Msg
+viewDrawer model =
+    div []
+        [ Button.render Mdl
+            [ 0 ]
+            model.mdl
+            [ Options.onClick (SetRoute CatalogRoute), css "margin" "0 24px" ]
+            [ text "Catalog" ]
+        , Button.render Mdl
+            [ 1 ]
+            model.mdl
+            [ Options.onClick (SetRoute ProfileRoute), css "margin" "0 24px" ]
+            [ text "Profile" ]
+        ]
+
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+    div
+        [ style
+            [ ( "text-align", "center" )
+            ]
+        ]
+        [ h4
+            [ style
+                [ ( "padding", "12px" )
+                , ( "margin", "0px" )
+                ]
+            ]
+            [ text "Growy" ]
+        ]
+
+
+viewBody : Model -> Html Msg
+viewBody model =
+    case model.route of
+        Routing.CatalogRoute ->
+            Catalog.view
+
+        Routing.ProfileRoute ->
+            text "this is the profile page place holder"
+
+        Routing.NotFoundRoute ->
+            text "404-ish - Not found!"
 
 
 view : Model -> Html Msg
 view model =
-    let
-        body =
-            case model.route of
-                Routing.CatalogRoute ->
-                    Catalog.view
-
-                Routing.ProfileRoute ->
-                    text "this is the profile page place holder"
-
-                Routing.NotFoundRoute ->
-                    text "404-ish - Not found!"
-    in
-        div []
-            [ Nav.view
-            , body
+    Layout.render Mdl
+        model.mdl
+        [ Layout.fixedHeader
+          --, Layout.fixedDrawer
+        ]
+        { header = [ viewHeader model ]
+        , drawer = [ viewDrawer model ]
+        , tabs = ( [], [] )
+        , main =
+            [ div
+                []
+                [ viewBody model ]
             ]
+        }
+        |> Material.Scheme.top
 
 
 main : Program Never Model Msg
