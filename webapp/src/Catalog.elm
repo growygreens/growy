66e14688a9f876aaa636502295220b468023d121
Anders Sundman <anders@4zm.org>
2017-03-15 10:12:19 +0100

    Split view with grid + close button

--- webapp/src/Catalog.elm
+++ webapp/src/Catalog.elm
@@ -4,6 +4,9 @@ import Domain exposing (..)
 import Html exposing (Html, div, text)
 import List
 import Material.Card as Card
+import Material.Button as Button
+import Material.Grid exposing (..)
+import Material.Icon as Icon
 import Material.List as List
 import Material.Options as Options exposing (css)
 import Phrases exposing (..)
@@ -40,23 +43,43 @@ selectedCultivarView model c =
             [ Card.head [] [ text c.name ]
             , Card.subhead [] [ text <| translatePlantType model c.plantType ]
             ]
+        , Card.menu []
+            [ Button.render Mdl
+                [ 0, 0 ]
+                model.mdl
+                [ Button.icon
+                , Button.ripple
+                , Options.onClick DismissSelectedCultivar
+                ]
+                [ Icon.i "close" ]
+            ]
         , Card.actions []
             [ text <| Maybe.withDefault (tr model Phrases.DescriptionMissing) c.description ]
         ]
+
+
+emptyNode : Html Msg
+emptyNode =
+    Html.text ""
 
 
 view : Model -> Html Msg
 view model =
     let
         cultivarList =
-            [ List.ul [] (List.map (itemView model) model.cultivars) ]
+            List.ul [] (List.map (itemView model) model.cultivars)
 
         selectedCultivar =
             case cultivarById model.selectedCultivar model of
                 Nothing ->
-                    []
+                    emptyNode
 
                 Just c ->
-                    [ selectedCultivarView model c ]
+                    selectedCultivarView model c
     in
-        div [] (cultivarList ++ selectedCultivar)
+        grid []
+            [ cell [ size All 6, size Tablet 4, size Phone 2 ]
+                [ cultivarList ]
+            , cell [ size All 6, size Tablet 4, size Phone 2 ]
+                [ selectedCultivar ]
+            ]
