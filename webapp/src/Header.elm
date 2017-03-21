8afc17d2a681e9141fbb024b4bca79259e58037a
Anders Sundman <anders@4zm.org>
2017-03-21 11:15:24 +0100

    Refactor header view

---
+++ webapp/src/Header.elm
@@ -0,0 +1,53 @@
+module Header exposing (viewHeader)
+
+import Html exposing (..)
+import Html.Attributes exposing (class, style)
+import Material.Button as Button
+import Material.Options as Options exposing (css, cs)
+import Domain exposing (Model, Msg(..))
+import Routing exposing (Route(..))
+
+
+headerButton : Model -> Int -> Msg -> String -> Html Msg
+headerButton model id msg buttonClass =
+    Button.render
+        Mdl
+        [ id ]
+        model.mdl
+        [ Options.onClick msg
+        , cs "header-button"
+        ]
+        [ i [ class buttonClass ] [] ]
+
+
+logo : Html Msg
+logo =
+    h4
+        [ style
+            [ ( "padding", "0 0 0 22px" )
+            , ( "margin", "0px" )
+            ]
+        ]
+        [ i
+            [ class "fa fa-leaf fa-flip-horizontal"
+            , style [ ( "padding", "0 0 0 16px" ) ]
+            ]
+            []
+        , text "Growy"
+        ]
+
+
+viewHeader : Model -> Html Msg
+viewHeader model =
+    div
+        -- Outer header row
+        [ class "header-row" ]
+        [ logo
+        , span [ style [ ( "flex-grow", "1" ) ] ] []
+        , span [ style [ ( "padding", "0 16px 0 0" ) ] ]
+            [ headerButton model 0 NoOp "fa fa-search"
+            , headerButton model 1 (SetRoute CatalogRoute) "fa fa-table"
+            , headerButton model 2 NoOp "fa fa-bookmark"
+            , headerButton model 3 (SetRoute ProfileRoute) "fa fa-user-circle"
+            ]
+        ]
--- webapp/src/ViewRoot.elm
+++ webapp/src/ViewRoot.elm
@@ -4,71 +4,11 @@ import Catalog exposing (view)
 import Domain exposing (Model, Msg(..))
 import Html exposing (..)
 import Html.Attributes exposing (href, class, style)
-import Material.Button as Button
 import Material.Layout as Layout
 import Material.Options as Options exposing (css, cs)
 import Material.Scheme
 import Routing exposing (Route(..))
-
-
-viewHeader : Model -> Html Msg
-viewHeader model =
-    div
-        -- Outer header row
-        [ style
-            [ ( "display", "flex" )
-            , ( "flex-direction", "row" )
-            , ( "align-items", "center" )
-            , ( "justify-content", "space-between" )
-            , ( "height", "100%" )
-            , ( "min-height", "64px" )
-            ]
-        ]
-        [ h4
-            [ style
-                [ ( "padding", "0 0 0 22px" )
-                , ( "margin", "0px" )
-                ]
-            ]
-            [ i
-                [ class "fa fa-leaf fa-flip-horizontal"
-                , style [ ( "padding", "0 0 0 16px" ) ]
-                ]
-                []
-            , text "Growy"
-            ]
-        , span [ style [ ( "flex-grow", "1" ) ] ] []
-        , span [ style [ ( "padding", "0 16px 0 0" ) ] ]
-            [ Button.render Mdl
-                [ 0 ]
-                model.mdl
-                [ Options.onClick NoOp
-                , cs "header-button"
-                ]
-                [ i [ class "fa fa-search" ] [] ]
-            , Button.render Mdl
-                [ 1 ]
-                model.mdl
-                [ Options.onClick (SetRoute CatalogRoute)
-                , cs "header-button"
-                ]
-                [ i [ class "fa fa-table" ] [] ]
-            , Button.render Mdl
-                [ 2 ]
-                model.mdl
-                [ Options.onClick NoOp
-                , cs "header-button"
-                ]
-                [ i [ class "fa fa-bookmark" ] [] ]
-            , Button.render Mdl
-                [ 3 ]
-                model.mdl
-                [ Options.onClick (SetRoute ProfileRoute)
-                , cs "header-button"
-                ]
-                [ i [ class "fa fa-user-circle" ] [] ]
-            ]
-        ]
+import Header exposing (viewHeader)
 
 
 viewBody : Model -> Html Msg
@@ -95,11 +35,7 @@ viewRoot model =
         , tabs = ( [], [] )
         , main =
             [ div
-                [ style
-                    [ ( "height", "100%" )
-                    , ( "overflow-y", "hidden" )
-                    ]
-                ]
+                [ class "main-box" ]
                 [ viewBody model ]
             ]
         }
