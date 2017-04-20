747ac177df338ad4b713d6f6eaf0a7bca1eb58c0
Anders Sundman <anders@4zm.org>
2017-04-20 22:53:15 +0200

    JSON Server for serving a fake DB

---
+++ webapp/api.js
@@ -0,0 +1,10 @@
+var jsonServer = require('json-server');
+
+var server = jsonServer.create();
+server.use(jsonServer.defaults());
+
+var router = jsonServer.router('mock-db.json');
+server.use(router);
+
+console.log('Mock DB Server API -- Listening at 4000');
+server.listen(4000);
