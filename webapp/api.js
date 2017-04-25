64b5740fa35ce4fc9f91a8b4dc2b29b17585f17e
Anders Sundman <anders@4zm.org>
2017-04-25 10:08:10 +0200

    Serve runabergs dataset (with images)

--- webapp/api.js
+++ webapp/api.js
@@ -3,7 +3,8 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 server.use(jsonServer.defaults());
 
-var router = jsonServer.router('mock-db.json');
+//var router = jsonServer.router('mock-db.json');
+var router = jsonServer.router('runabergs.json');
 server.use(router);
 
 console.log('Mock DB Server API -- Listening at 4000');
